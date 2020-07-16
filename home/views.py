01d1888c87e3569e2ce71bed4db76d8ab8b067d8
ChiefChingu <sangho.grolleman@gmail.com>
2020-07-16 18:14:32 +0200

    Add homepage items.

--- home/views.py
+++ home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    featured_books = Book.objects.filter(featured_item = True)
+    recent_books = Book.objects.filter(date_added = "2020-07-16")
+
+    context = {
+        'featured_books': featured_books,
+        'recent_books': recent_books,
+        # 'search_term': query,
+    }
+
+    return render(request, 'home/index.html', context)

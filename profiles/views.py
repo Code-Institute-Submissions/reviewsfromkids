1bb9d7526f9faf1b61961572b01e4dacb5411893
ChinguSangho <sangho.grolleman@gmail.com>
2020-08-11 14:50:36 +0200

    Add ratings as book cards to profile.

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
-from books.models import Rating
+from books.models import Rating, Book
 
 # Create your views here.
 def profile(request):
@@ -10,15 +10,19 @@ def profile(request):
     hobby = profile.hobbies.all().order_by('name')
     sport = profile.sports.all().order_by('name')
 
-    ratings = Rating.objects.filter(rated_by=profile)
-    print(ratings)
+    ratings_high = Book.objects.filter(rating__rated_by=profile, rating__rating__gte=4)
+    ratings_low = Book.objects.filter(rating__rated_by=profile, rating__rating__lte=2)
+    ratings_ok = Book.objects.filter(rating__rated_by=profile, rating__rating=3)
+
 
     template = 'profiles/profile.html'
     context = {
         'hobby':hobby,
         'sport': sport,
         'profile': profile,
-        'ratings': ratings,
+        'ratings_high': ratings_high,
+        'ratings_low': ratings_low,
+        'ratings_ok': ratings_ok,
         }
 
     return render(request, template, context)

820a1bfc86c2e13b1b784d1523d824890d68fac5
ChinguSangho <sangho.grolleman@gmail.com>
2020-08-12 10:31:24 +0200

    Add profile edit form.

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('add_personal', views.add_personal, name='add_personal')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -26,3 +26,44 @@ def profile(request):
         }
 
     return render(request, template, context)
+
+def add_personal(request):
+    """ User adds personal information to profile """
+
+    # Get userprofile
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    # Grab POST values
+    if request.POST:
+        user = profile
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        gender = request.POST.get('gender')
+        date_of_birth = request.POST.get('date_of_birth')
+        print(profile.pk)
+
+    # Store POST values in record
+    UserProfile.objects.filter(id=profile.pk).update(
+        first_name=first_name,
+        last_name=last_name,
+        gender=gender,
+        date_of_birth=date_of_birth,
+    )
+
+    template = 'profiles/profile.html'
+    context = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'gender': gender,
+        'date_of_birth': date_of_birth,
+    }
+
+    return render(request, template, context)
+
+# def add_sport(request):
+#     """ User can edit his/her hobbies """
+    
+#     sports = profile.sports.all().order_by('name')
+
+
+

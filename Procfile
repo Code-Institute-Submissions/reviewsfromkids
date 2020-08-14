2e593a112d051cd9d4c90db7b37558639f405760
ChiefChingu <sangho.grolleman@gmail.com>
2020-08-14 10:26:30 +0200

    Add procfile, update settings for heroku.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn reviewsfromkids.wsgi:application
--- reviewsfromkids/settings.py
+++ reviewsfromkids/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['reviewsfromkids.herokuapp.com', 'localhost']
 
 
 # Application definition

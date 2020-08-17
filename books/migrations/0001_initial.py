1a6b4b7a2d2b1d3a2ad3390004f6028bff928079
ChinguSangho <sangho.grolleman@gmail.com>
2020-08-17 17:04:29 +0200

    Remove comments, comment out tag model.

--- books/migrations/0001_initial.py
+++ books/migrations/0001_initial.py
@@ -31,14 +31,14 @@ class Migration(migrations.Migration):
                 ('friendly_name', models.CharField(blank=True, max_length=254, null=True)),
             ],
         ),
-        migrations.CreateModel(
-            name='Tag',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=254)),
-                ('friendly_name', models.CharField(blank=True, max_length=254, null=True)),
-            ],
-        ),
+        # migrations.CreateModel(
+        #     name='Tag',
+        #     fields=[
+        #         ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+        #         ('name', models.CharField(max_length=254)),
+        #         ('friendly_name', models.CharField(blank=True, max_length=254, null=True)),
+        #     ],
+        # ),
         migrations.CreateModel(
             name='Book',
             fields=[

32c9b559814ca81337ca28f69a4190bb3149610b
ChiefChingu <sangho.grolleman@gmail.com>
2020-08-18 08:58:45 +0200

    Introduce modelforms at profiles. Move to other laptop to continue.

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.forms import ModelForm
 
 GENDER_CHOICES = [
     ('GIRL', 'girl'),
@@ -34,6 +35,11 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
     instance.userprofile.save()
+
+# class UserProfileForm(ModelForm):
+#     class Meta:
+#         model = UserProfile
+#         fields = ['first_name', 'last_name', 'gender', 'date_of_birth', 'hobbies', 'sports']
 
 class Hobby(models.Model):
 

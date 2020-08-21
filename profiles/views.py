193dcc20f07adb94e97e536a7f517404ce12943c
ChinguSangho <sangho.grolleman@gmail.com>
2020-08-21 16:22:09 +0200

    Add flow for user to change/update hobbies and sports and come back to bookfinder.

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import *
-from books.models import Rating, Book
+from books.models import Rating, Book, Category
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 # from .forms import *
@@ -100,7 +100,7 @@ def edit_sport(request):
             form.save()
             return redirect('profile')
 
-    # Load edit hobby page
+    # Load edit sport page
     template = 'profiles/edit_sport.html'
     context = {'form': form}
     return render(request, template, context)
@@ -383,33 +383,79 @@ def book_finder_user_3(request):
 def book_finder_user_4(request):
     """ Display user profile """
 
-    hobby=Hobby.objects.all()
-    sport=Sport.objects.all()
     user = get_object_or_404(User, username=request.user)
-
     profile = get_object_or_404(UserProfile, user=request.user)
     user_hobby = profile.hobbies.all().order_by('name')
     user_sport = profile.sports.all().order_by('name')
-    favorites = profile.favorites.all()
-
-    ratings_high = Book.objects.filter(rating__rated_by=profile, rating__rating__gte=4)
-    ratings_low = Book.objects.filter(rating__rated_by=profile, rating__rating__lte=2)
-    ratings_ok = Book.objects.filter(rating__rated_by=profile, rating__rating=3)
-
-
-    template = 'profiles/profile.html'
-
+    categories = Category.objects.all()
+    
     context = {
         'user': user,
-        'hobby': hobby,
-        'sport': sport,
         'user_hobby':user_hobby,
         'user_sport': user_sport,
         'profile': profile,
-        'ratings_high': ratings_high,
-        'ratings_low': ratings_low,
-        'ratings_ok': ratings_ok,
-        'favorites': favorites,
         }
     
     return render(request, 'profiles/book_finder_user_4.html', context)
+
+
+@login_required
+def book_finder_user_5(request):
+    """ Display user profile """
+
+    user = get_object_or_404(User, username=request.user)
+    profile = get_object_or_404(UserProfile, user=request.user)
+    user_hobby = profile.hobbies.all().order_by('name')
+    user_sport = profile.sports.all().order_by('name')
+    categories = Category.objects.all()
+    print(categories)
+
+    context = {
+        'user': user,
+        'user_hobby':user_hobby,
+        'user_sport': user_sport,
+        'profile': profile,
+        }
+    
+    return render(request, 'profiles/book_finder_user_5.html', context)
+
+def book_finder_edit_hobby(request):
+    """ User can add his/her hobbies """
+    
+    # Get userprofile
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    # Render existing data on form
+    form = UserProfileHobbyForm(instance=profile)
+
+    if request.method == "POST":
+        form = UserProfileHobbyForm(instance=profile, data = request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_finder_user_5')
+
+    # Load edit hobby page
+    template = 'profiles/book_finder_edit_hobby.html'
+    context = {'form': form}
+    return render(request, template, context)
+
+
+def book_finder_edit_sport(request):
+    """ User can add his/her sports """
+    
+    # Get userprofile
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    # Render existing data on form
+    form = UserProfileSportForm(instance=profile)
+
+    if request.method == "POST":
+        form = UserProfileSportForm(instance=profile, data = request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_finder_user_5')
+
+    # Load edit sport page
+    template = 'profiles/book_finder_edit_sport.html'
+    context = {'form': form}
+    return render(request, template, context)

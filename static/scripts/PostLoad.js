e0f18a160e647fbd3dfeba46174c5a18c51a8637
ChinguSangho <sangho.grolleman@gmail.com>
2020-08-25 18:05:00 +0200

    Move postload js to separate file.

---
+++ static/scripts/PostLoad.js
@@ -0,0 +1,23 @@
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+if (prevScrollpos > currentScrollPos) {
+    showHeader();
+} else {
+    
+    hideHeader();
+}
+prevScrollpos = currentScrollPos;
+}
+
+function hideHeader() {
+    $(".toggle").removeClass("is-visible").addClass("is-hidden");
+}
+
+function showHeader() {
+    $(".toggle").removeClass("is-hidden").addClass("is-visible").addClass("scrolling");
+}
+
+
+$('.toast').toast('show');
